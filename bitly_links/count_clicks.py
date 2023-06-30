4dad3920a0e565f60ed61852406d14858698c4eb
Alayna19 <kuznetsova.a23@gmail.com>
2023-06-30 17:03:31 +0300

    corrected script for assessment
    1) removed brackets, removed _list, _dict, _str, _json from variables names
    2) changed all double quotes to single quotes
    3) turned  "\n" + string + "\n" into f-strings
    4) fixed pep8 code formatting issues
    5) added support of non-bitly domains

--- bitly_links/count_clicks.py
+++ bitly_links/count_clicks.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-The script turns long links into shortend links like 
-ex. https://www.google.com/search?q=how+to+be+cool 
+The script turns long links into shortend links like
+ex. https://www.google.com/search?q=how+to+be+cool
 => https://bit.ly/46lV7aP
-If the link already exists, the script returns how many times people 
+If the link already exists, the script returns how many times people
 clicked on it:
 ex. Total clicks on https://bit.ly/46lV7aP:  11
 
@@ -16,19 +16,18 @@ import requests
 token = os.getenv('BITLY_TOKEN')
 
 
-def shorten_link(token, url):
-    target_url = 'https://api-ssl.bitly.com/v4/shorten'
-    headers = {'Authorization': 'Bearer {}'.format(token)}
-    payload = {'long_url': url}
-    response = requests.post(target_url, headers=headers, json=payload)
+def shorten_link(token, link):
+    url = 'https://api-ssl.bitly.com/v4/shorten'
+    headers = {'Authorization': f'Bearer {token}'}
+    payload = {'long_url': link}
+    response = requests.post(url, headers=headers, json=payload)
     response.raise_for_status()
     response = response.json()
     return response['link']
 
 
 def count_clicks(token, bitlink):
-    bitlink = bitlink.replace('http://', '').replace('https://', '')
-    headers = {'Authorization': 'Bearer {}'.format(token)}
+    headers = {'Authorization': f'Bearer {token}'}
     params = {'units': '-1', 'unit': 'day', }
     url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
     response = requests.get(url, headers=headers, params=params)
@@ -38,21 +37,18 @@ def count_clicks(token, bitlink):
 
 def main():
     link = input('Enter a link: ')
-    parsed = urlparse(link)
-    full_path = (
-        f'https://api-ssl.bitly.com/v4/bitlinks/{parsed.netloc}/{parsed.path}'
-    )
-    resp = requests.get(full_path, headers={'Authorization': f'Bearer {token}'})
+    trimmed_link = f'{urlparse(link).netloc}{urlparse(link).path}'
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{trimmed_link}'
+    resp = requests.get(url, headers={'Authorization': f'Bearer {token}'})
     try:
         if resp.ok:
-            print(link)
-            number_of_clicks = count_clicks(token, link)
+            number_of_clicks = count_clicks(token, trimmed_link)
             result = f'\nTotal clicks on {link}:  {number_of_clicks} \n'
         else:
             link = shorten_link(token, link)
-            result = f'\n Short link: {link} \n'
+            result = f'\nShort link: {link} \n'
     except requests.exceptions.HTTPError:
-        result = "\nThe link you entered is incorrect or API isn't responding"
+        result = "\nThe link you entered is incorrect or API isn't responding\n"
     print(result)
 
 
